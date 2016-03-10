ee26020d8d4f3a676758974d0e16f581278cadc1
abrahd2 <deepak.abraham@emc.com>
2016-03-10 12:59:07 +0530

    spring boot rest service example

---
+++ spring-boot-rest/src/main/java/com/deepak/springbootrest/Application.java
@@ -0,0 +1,19 @@
+package com.deepak.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Run with mvn spring-boot:run or java -jar target/<name of jar> Run with
+ * http://localhost:8080/greeting
+ * 
+ * @author abrahd2
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
