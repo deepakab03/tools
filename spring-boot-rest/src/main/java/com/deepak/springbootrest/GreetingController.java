b1bbe1d3036631ad851cdc4ae82a816c6cd25953
abrahd2 <deepak.abraham@emc.com>
2016-04-25 17:41:25 +0530

    logged each request

--- spring-boot-rest/src/main/java/com/deepak/springbootrest/GreetingController.java
+++ spring-boot-rest/src/main/java/com/deepak/springbootrest/GreetingController.java
@@ -2,19 +2,25 @@ package com.deepak.springbootrest;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 	
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+    	counter.incrementAndGet();
+    	logger.info("Got a request with name {}, number {}", name, counter.get());
+        return new Greeting(counter.get(),
                             String.format(template, name));
     }
 }
