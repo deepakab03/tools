42e022c51d600b4f69913d6c78c1f52a23923448
abrahd2 <deepak.abraham@emc.com>
2017-10-18 15:56:07 +0530

    small changes

--- log-output-creator/src/main/java/com/deepak/tools/creator/LogOutputCreatorController.java
+++ log-output-creator/src/main/java/com/deepak/tools/creator/LogOutputCreatorController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +20,55 @@ public class LogOutputCreatorController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/loglines")
-    public LogCount greeting(@RequestParam(value="kiloLines", defaultValue="10") int numKiloLines) {
-    	counter.incrementAndGet();
-    	logger.info("Got a request with kiloLines {}, number {}", numKiloLines, counter.get());
-    	int numLines = numKiloLines * 1000;
+    public LogCount logToOutput(@RequestParam(value="numLines", defaultValue="10") int numLinesPassedIn) {
+       return logToStdOut(numLinesPassedIn);
+    }
+    
+    @RequestMapping("/out/loglines")
+    public LogCount logToStdOut(@RequestParam(value="numLines", defaultValue="10") int numLinesPassedIn) {
+    	int numLines = validateAndPrintLog(numLinesPassedIn);
     	for (int i = 0; i< numLines; i++) {
     	    logger.info("{}", LINE);
     	}
+        return logCounter(numLines);
+    }
+
+    private int validateAndPrintLog(int numLinesPassedIn) {
+        Assert.isTrue(numLinesPassedIn > 0, numLinesPassedIn + " has to be greater than 0");
+        counter.incrementAndGet();
+    	logger.info("Got a request with numLines {}, number {}", numLinesPassedIn, counter.get());
+        return numLinesPassedIn;
+    }
+    
+    private LogCount logCounter(int numLines) {
         return new LogCount(counter.get(),
-                            String.format(template, numLines));
+                String.format(template, numLines));
+    }
+    
+    @RequestMapping("/out/loglines/debug")
+    public LogCount logToStdOutAtDebug(@RequestParam(value="numLines", defaultValue="10") int numLinesPassedIn) {
+        int numLines = validateAndPrintLog(numLinesPassedIn);
+        for (int i = 0; i< numLines; i++) {
+            logger.debug("{}", LINE);
+        }
+        return logCounter(numLines);
+    }
+    
+    @RequestMapping("/out/printlines")
+    public LogCount printoStdOut(@RequestParam(value="numLines", defaultValue="10") int numLinesPassedIn) {
+        int numLines = validateAndPrintLog(numLinesPassedIn);
+        for (int i = 0; i< numLines; i++) {
+           System.out.println(LINE);
+        }
+        return logCounter(numLines);
+    }
+    
+    @RequestMapping("/err/printlines")
+    public LogCount printToStdErr(@RequestParam(value="numLines", defaultValue="10") int numLinesPassedIn) {
+        int numLines = validateAndPrintLog(numLinesPassedIn);
+        for (int i = 0; i< numLines; i++) {
+            System.err.println(LINE);
+        }
+        return logCounter(numLines);
     }
 }
